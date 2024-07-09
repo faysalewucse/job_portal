c6c46504e69fe947cc08c66faa3b7b9b551c5f6d
faysal-zaag <faysal@zaagsys.com>
2024-07-09 15:16:46 +0600

    add all jobs page

--- src/pages/jobs/AllJobs.jsx
+++ src/pages/jobs/AllJobs.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, Typography, Grid, Card, CardContent } from "@mui/material";
+import { useGetJobsQuery } from "../api/jobApi";
 
 const AllJobs = () => {
-  return <div>AllJobs</div>;
+  const { data: jobs, error, isLoading, isError, refetch } = useGetJobsQuery();
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  if (isLoading) return <Typography variant="h5">Loading...</Typography>;
+  if (isError)
+    return <Typography variant="h5">Error: {error.message}</Typography>;
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        All Jobs
+      </Typography>
+      <Grid container spacing={3}>
+        {jobs.map((job) => (
+          <Grid item xs={12} sm={6} md={4} key={job.id}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">{job.title}</Typography>
+                <Typography variant="body1">{job.description}</Typography>
+                {/* Add more job details as needed */}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default AllJobs;
